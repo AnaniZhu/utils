80a8479f14694ef3fbfad2d73e565aaf55aff9fb
zhuwenhan <1945985458@qq.com>
2018-03-19 16:57:26 +0800

    update cookie.js

--- business/cookie.js
+++ business/cookie.js
@@ -1,3 +1,30 @@
-export function getCookie (name) {
-  s
+export const cookie = {
+	get (name) {
+	  let cookies = document.cookie.split('; ');
+	  for (let cookie of cookies) {
+	  	let [key, value] = cookie.split('=');
+	  	if (key === name) {
+	  		return value;
+	  	}
+	  }
+	},
+	set (name, value, exDays) {
+		let cookie = name + '=' + value;
+		if (exDays) {
+			let d = new Date();
+			d.setTime(+d + exDays * 1000 * 3600 * 24);
+			cookie +=  ';expires=' + d.toGMTString();
+		}
+		document.cookie = cookie;
+	},
+	remove (name) {
+		this.set(name, '', 0.000000000001);
+	},
+	parse() {
+		return document.cookie.split('; ').reduce((pre, next) => {
+			let [key, value] = next.split('=');
+			pre[key] = value;
+			return pre;
+		}, {});
+	}
 }
